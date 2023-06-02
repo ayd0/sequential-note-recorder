12ee101703191cdf09b98ea1425f20d802dfe94a
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-06-01 18:52:48 -0700

    finished UI template

--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,7 @@ import {} from "./web-components/layout-components";
 const App = () => {
     return (
         <div id="app" style="display: flex; flex-flow: column;">
-            <switcher-l
-                space="var(--border-thin);"
-            >
+            <switcher-l space="var(--border-thin);">
                 <box-l padding="var(--s-5)" style="height: var(--nav-height)">
                     <h2>1</h2>
                 </box-l>
@@ -42,15 +40,32 @@ const App = () => {
                         sideWidth="calc(10rem + 2 * var(--s1))"
                         style="height: 100%;"
                     >
-                        <box-l style="height: 100%; padding-block: 0;"></box-l>
+                        <sidebar-l
+                            side="right"
+                            contentMin="47.5%"
+                            space="var(--border-thin)"
+                            sideWidth="43%"
+                            style="height: 100%; overflow-y: auto;"
+                        >
+                            <stack-l
+                                space="var(--border-thin)"
+                                style="display: flex; flex-flow: column; overflow: hidden;"
+                            >
+                                <box-l></box-l>
+                                <box-l style="flex-grow: 1"></box-l>
+                                <box-l style="flex-grow: 1"></box-l>
+                            </stack-l>
+                            <box-l style="padding-block: 0; flex-grow: 1">
+                                <h1>Steps</h1>
+                            </box-l>
+                        </sidebar-l>
                         <box-l
                             class="timer-container"
                             style="height: 100%; padding-block: 0; overflow: hidden; padding-inline: 0;"
                         >
                             <center-l andText style="height: 100%">
                                 <h1>Pomodoro</h1>
-                                <stack-l style="height: 100%; overflow-y: auto;">
-                                </stack-l>
+                                <stack-l style="height: 100%; overflow-y: auto;"></stack-l>
                             </center-l>
                         </box-l>
                     </sidebar-l>
