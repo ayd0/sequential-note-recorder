6a24651aa468200bde4e707da1f06db5540bf5ba
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-06-04 10:32:21 -0700

    basic structure of navbar

--- src/components/app.js
+++ src/components/app.js
@@ -8,18 +8,33 @@ const App = () => {
         <div id="app">
             <switcher-l space="var(--border-thin);" threshold="30rem">
                 <box-l class="box-style:nav" padding="var(--s-5)">
-                    <div style="display: flex; align-items: center; width: 100%:">
+                    <div style="display: flex; align-items: center; width: 100%; height: var(--nav-height)">
                         <ReactSVG
                             src="../assets/icons/step-over.svg"
                             style="opacity: 80%; margin-inline-end: 2ch"
                         />
                         <div id="search-container">
-                            <input style="flex-grow: 1;"></input>
-                            <button>Test</button>
+                            <ReactSVG
+                                src="../assets/icons/search.svg"
+                                style="opacity: 80%;"
+                            />
+                            <input type="text" placeholder="Search"></input>
+                            <button>Search</button>
                         </div>
+                        <div style="flex-grow: 1;"></div>
                     </div>
                 </box-l>
-                <box-l class="box-style:nav" padding="var(--s-5)"></box-l>
+                <box-l class="box-style:nav" padding="var(--s-5)">
+                    <div
+                        style="display: flex; justify-content: flex-end; align-items: center; height: 100%;"
+                        class="fade-all"
+                    >
+                        <ReactSVG src="../assets/icons/dark-mode.svg" />
+                        <ReactSVG src="../assets/icons/widgets.svg" />
+                        <ReactSVG src="../assets/icons/settings.svg" />
+                        <ReactSVG src="../assets/icons/account-circle.svg" />
+                    </div>
+                </box-l>
             </switcher-l>
             <sidebar-l contentMin="0%" space="0" class="sidebar-style:main">
                 <div id="nav-drawer-container">
