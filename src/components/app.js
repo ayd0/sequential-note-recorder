9836d6e7c3d15a46f83884520b958095c40649c2
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-06-08 19:50:24 -0700

    added context colors, adjusted styling of timer

--- src/components/app.js
+++ src/components/app.js
@@ -233,7 +233,7 @@ const App = () => {
                                         </icon-l>
                                     </p>
                                 </div>
-                                <div class="step-entry:open">
+                                <div class="step-entry:open step-entry:edit">
                                     <switcher-l
                                         style="align-items: center"
                                         threshold="1rem"
@@ -288,15 +288,21 @@ const App = () => {
                                     space="var(--s-5)"
                                 >
                                     <div>
-                                        <p style="display: flex; align-items: center; margin-block-end: 0;">
+                                        <h4 style="display: flex; align-items: center; margin-block: 0;">
                                             Break{" "}
                                             <icon-l>
                                                 <ReactSVG
                                                     style="font-size: 2.4em; padding-inline-start: var(--s-3); padding-block-start: var(--s-3);"
+                                                    src="../assets/icons/timer.svg"
+                                                />
+                                            </icon-l>
+                                            <icon-l>
+                                                <ReactSVG
+                                                    style="font-size: 2.4em; auto; padding-inline-end: var(--s-1); padding-block-start: var(--s-3);"
                                                     src="../assets/icons/skip.svg"
                                                 />
                                             </icon-l>
-                                        </p>
+                                        </h4>
                                         <p style="display: flex; align-items: center; margin-block-start: 0;">
                                             00:25 / 15:00{" "}
                                             <icon-l>
@@ -308,26 +314,26 @@ const App = () => {
                                             2:00
                                         </p>
                                     </div>
-                                    <p style="display: flex;">
+                                    <p style="display: flex; align-items: center;">
                                         Study: 26:00
-                                        <icon-l style="margin-inline: 1ch;">
+                                        <icon-l style="margin-inline: .2ch; font-size: 2em; fill: green;">
                                             <ReactSVG src="../assets/icons/plus.svg" />
                                         </icon-l>
-                                        1:00
+                                        <span style="border-bottom: 1px solid green">1:00</span>
                                     </p>
-                                    <p style="display: flex;">
+                                    <p style="display: flex; align-items: center">
                                         Break: 12:00
-                                        <icon-l style="margin-inline: 1ch;">
+                                        <icon-l style="margin-inline: .2ch; font-size: 2em; fill: red;">
                                             <ReactSVG src="../assets/icons/minus.svg" />
                                         </icon-l>
-                                        3:00
+                                        <span style="border-bottom: 1px solid red">3:00</span> 
                                     </p>
-                                    <p style="display: flex;">
-                                        Break: 12:00
-                                        <icon-l style="margin-inline: 1ch;">
+                                    <p style="display: flex; align-items: center;">
+                                        Study: 22:50
+                                        <icon-l style="margin-inline: .2ch; font-size: 2em; fill: orange;">
                                             <ReactSVG src="../assets/icons/minus.svg" />
                                         </icon-l>
-                                        3:00
+                                        <span style="border-bottom: 1px solid orange">2:10</span>
                                     </p>
                                 </stack-l>
                             </center-l>
